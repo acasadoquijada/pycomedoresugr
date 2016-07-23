107301f9d6d0568fbb7b5d0759ef76685296df78
acasadoquijada <acasadoquijada@gmail.com>
2016-07-23 17:25:00 +0200

    Mejorando tests #1

--- tests.py
+++ tests.py
@@ -8,14 +8,13 @@ class Test(unittest.TestCase):
 
     def test_get_menu_semana(self):
         
-        menu_semana_obtenido = get_menu_semana("Test/test.html")
-        
+        menu_semana_obtenido = get_menu_semana()        
         
         self.assertEqual(menu_semana_obtenido, menu_semana)
 
     def test_menu_semana_diccionario(self):
     
-        menu_semana_diccionario_obtenido = menu_semana_diccionario("Test/test.html")
+        menu_semana_diccionario_obtenido = menu_semana_diccionario()
         
         self.assertEqual(menu_semana_diccionario_info, menu_semana_diccionario_obtenido)
 
