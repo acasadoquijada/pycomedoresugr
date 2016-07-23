868806b4361c54a10e97c7c14525f39d11790c9a
acasadoquijada <acasadoquijada@gmail.com>
2016-07-23 18:06:24 +0200

    Añadida modificación resultante #1

--- info_tests.py
+++ info_tests.py
@@ -1,4 +1,14 @@
+#Información necesaria para los tests
+
 menu_semana = [' LUNES 27 JUNIO ', 'Raviolis a la Putanesca', 'Escalope de Cerdo con Ensalada Cortijera', 'Pan, Vino Tinto y Sandía', ' MARTES 28 JUNIO ', 'Arroz a la Cubana', 'Fritura de Pescado con Ensalada Col', 'Pan, Refresco y Plátano', ' MIÉRCOLES 29 JUNIO ', 'Pizza', 'Filete de Cerdo a la Parrilla con Pimientos Fritos', 'Pan, Vino Tinto y Melocotón', ' JUEVES 30 JUNIO ', ' CERRADO ', ' VIERNES 01 JULIO ', ' CERRADO ', ' SÁBADO 02 JULIO ', ' CERRADO ']
 
 
 menu_semana_diccionario_info = {'JUEVES': [' JUEVES 30 JUNIO ', ' CERRADO '], 'LUNES': [' LUNES 27 JUNIO ', 'Raviolis a la Putanesca', 'Escalope de Cerdo con Ensalada Cortijera', 'Pan, Vino Tinto y Sandía'], 'SÁBADO': [' SÁBADO 02 JULIO ', ' CERRADO '], 'VIERNES': [' VIERNES 01 JULIO ', ' CERRADO '], 'MARTES': [' MARTES 28 JUNIO ', 'Arroz a la Cubana', 'Fritura de Pescado con Ensalada Col', 'Pan, Refresco y Plátano'], 'MIÉRCOLES': [' MIÉRCOLES 29 JUNIO ', 'Pizza', 'Filete de Cerdo a la Parrilla con Pimientos Fritos', 'Pan, Vino Tinto y Melocotón']}
+
+
+menu_semana_json_info = {'JUEVES': [' JUEVES 30 JUNIO ', ' CERRADO '], 'LUNES': [' LUNES 27 JUNIO ', 'Raviolis a la Putanesca', 'Escalope de Cerdo con Ensalada Cortijera', 'Pan, Vino Tinto y Sandía'], 'MARTES': [' MARTES 28 JUNIO ', 'Arroz a la Cubana', 'Fritura de Pescado con Ensalada Col', 'Pan, Refresco y Plátano'], 'VIERNES': [' VIERNES 01 JULIO ', ' CERRADO '], 'MIÉRCOLES': [' MIÉRCOLES 29 JUNIO ', 'Pizza', 'Filete de Cerdo a la Parrilla con Pimientos Fritos', 'Pan, Vino Tinto y Melocotón'], 'SÁBADO': [' SÁBADO 02 JULIO ', ' CERRADO ']}
+
+menu_dia_info= [' LUNES 27 JUNIO ', 'Raviolis a la Putanesca', 'Escalope de Cerdo con Ensalada Cortijera', 'Pan, Vino Tinto y Sandía']
+
+
+
